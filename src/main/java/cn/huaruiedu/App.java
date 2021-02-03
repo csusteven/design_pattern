05a5d0492c3d21e65f86e78ee44d99fcbe0f6d75
steven <shengming_boai@163.com>
2021-02-03 10:43:35 +0800

    dev使用tag第一次提交

--- src/main/java/cn/huaruiedu/App.java
+++ src/main/java/cn/huaruiedu/App.java
@@ -8,5 +8,6 @@ public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
         System.out.println("增加分支");
+        System.out.println("使用dev分支提交并且增加");
     }
 }
